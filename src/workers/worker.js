974218f49e4eb256386cdc99269255aea8ded376
Oğuz Utku Yıldız <oguzutku1745@gmail.com>
2023-11-13 20:28:09 +0300

    First commit for aleo app, uses Hamp's explorer

---
+++ src/workers/worker.js
@@ -0,0 +1,74 @@
+import {
+  Account,
+  ProgramManager,
+  PrivateKey,
+  initThreadPool,
+  AleoKeyProvider,
+  AleoNetworkClient,
+  NetworkRecordProvider,
+} from "@aleohq/sdk";
+import { expose, proxy } from "comlink";
+
+await initThreadPool();
+
+async function localProgramExecution(program, aleoFunction, inputs) {
+  const programManager = new ProgramManager();
+
+  // Create a temporary account for the execution of the program
+  const account = new Account();
+  programManager.setAccount(account);
+
+  const executionResponse = await programManager.executeOffline(
+    program,
+    aleoFunction,
+    inputs,
+    false,
+  );
+  return executionResponse.getOutputs();
+}
+
+async function getPrivateKey() {
+  const key = new PrivateKey();
+  return proxy(key);
+}
+
+async function deployProgram(program) {
+  const keyProvider = new AleoKeyProvider();
+  keyProvider.useCache(true);
+
+  // Thanks to Hamp: https://discord.com/channels/700454073459015690/998986493999726632/1173638409873522760
+
+  // Create a record provider that will be used to find records and transaction data for Aleo programs
+  const networkClient = new AleoNetworkClient("https://explorer.hamp.app/");
+
+  // Use existing account with funds
+  const account = new Account({
+    privateKey: "user1PrivateKey (I removed this part)",
+  });
+
+  const recordProvider = new NetworkRecordProvider(account, networkClient);
+
+  // Initialize a program manager to talk to the Aleo network with the configured key and record providers
+  const programManager = new ProgramManager(
+    "https://explorer.hamp.app",
+    keyProvider,
+    recordProvider,
+  );
+
+  programManager.setAccount(account);
+
+  // Define a fee to pay to deploy the program
+  const fee = 1.9; // 1.9 Aleo credits
+
+  // Deploy the program to the Aleo network
+  //const tx_id = await programManager.deploy(program, fee);
+
+  // Optional: Pass in fee record manually to avoid long scan times
+  const feeRecord = "{  owner: aleo1cwkh70c7uz3ssz65w4f04pphux7es4xw6l5fqsufx5rqgcjt4qqqrzy9gf.public,  microcredits: 20000000u64.public,  _nonce: 2842454941775057573577032029044965661575449057305471586457839348415512389703group.public}";
+  const tx_id = await programManager.deploy(program, fee, undefined, feeRecord);
+
+  return tx_id;
+}
+
+const workerMethods = { localProgramExecution, getPrivateKey, deployProgram };
+expose(workerMethods);
